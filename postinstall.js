ba53f2f75bd6f36d33ec78f5856b856b196efee9
Kirill Danshin <k@guava.by>
2017-07-23 22:47:24 +0300

    reword error message
    Signed-off-by: Kirill Danshin <k@guava.by>

--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,1 @@
-console.log('\x1b[31m', '"nuclein" is not true name!!! run "npm i nuklein"', '\x1b[0m');
+console.log('\x1b[31m', '"nuclein" isn\'t a right name. Use "npm i -S nuklein" instead', '\x1b[0m');
